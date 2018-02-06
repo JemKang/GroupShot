33430757706f062b222f678dcadbd4d0365b656b
kjh <kjh@meitu.com>
2018-02-06 09:13:45 +0800

    添加首页UI

---
+++ GroupShot/GroupShot/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  GroupShot
+//
+//  Created by kjh on 2018/2/6.
+//  Copyright © 2018年 kjh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class ViewController;
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) ViewController *viewController;
+
+
+@end
+
---
+++ GroupShot/GroupShot/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  GroupShot
+//
+//  Created by kjh on 2018/2/6.
+//  Copyright © 2018年 kjh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(NSUInteger)supportedInterfaceOrientations{
+    return (NSUInteger)UIInterfaceOrientationMaskLandscape;
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
